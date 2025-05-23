ca959e34d91385b1f0828fb876025f611cdbd20d
Cameron Gillette <camerongillette@gmail.com>
2025-05-22 20:10:43 -0500

    Removed console log

--- src/SleepToken.tsx
+++ src/SleepToken.tsx
@@ -5,7 +5,7 @@ import './App.css';
 import { logGeneratedTopic, logPageView } from './utils/analytics';
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
-console.log(backendUrl); 
+//console.log(backendUrl); 
 
 // ---- DATA TYPES
 type Topic = {
